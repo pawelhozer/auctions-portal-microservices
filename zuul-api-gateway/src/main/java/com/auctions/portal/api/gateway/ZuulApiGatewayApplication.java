7f1420539a2e08e072f90d0da7c29bcb0f3126c9
pawelhozer <pawelhozer@gmail.com>
2020-03-02 19:06:15 +0100

    init commit

---
+++ zuul-api-gateway/src/main/java/com/auctions/portal/api/gateway/ZuulApiGatewayApplication.java
@@ -0,0 +1,10 @@
+package com.auctions.portal.api.gateway;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableZuulProxy
+public class ZuulApiGatewayApplication {
+
+}
